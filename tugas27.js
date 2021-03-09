94d288b5193cf13789558a503474150a9533e662
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-09 14:16:17 +0700

    Tugas 27 JS
    Tugas Niomic

---
+++ tugas27.js
@@ -0,0 +1,19 @@
+const tugas27 = () => {
+    //no.1
+    var a = 'No';
+    var b = 'rumah saya';
+    var c = 30;
+    var d = 'A';
+
+    hasil = a+' '+b+' '+c+d;
+    console.log(hasil);
+
+    //no2
+    var x = 'Jalan pangeran Tirtayasa No'
+    var y = 70;
+
+    hasil1 = x+' '+y;
+    console.log(hasil1);
+}
+
+tugas27();
